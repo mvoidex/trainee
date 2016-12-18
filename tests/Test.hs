745b70b5e627c06db00c9b33a5e7d2bf02c309e4
voidex <voidex@live.com>
2016-12-19 02:14:58 +0300

    Experiment functions in test

--- tests/Test.hs
+++ tests/Test.hs
@@ -1,16 +1,20 @@
 module Main (
-	main
+	main,
+	trainUntil', learnUnary, learnBinary
 	) where
+
+import Prelude.Unicode
 
 import Control.Concurrent
 import Control.Monad
+import Control.Monad.Loops
 import Control.Monad.State
 
 import Numeric.Trainee.Neural
 import Numeric.LinearAlgebra
 
-test ∷ IO (Net Double)
-test = net $ input 2 ⭃ fc sigma 2 ⭃ fc sigma 2 ⭃ fc sigma 1
+nnet ∷ IO (Net Double)
+nnet = net $ input 2 ⭃ fc sigma 2 ⭃ fc sigma 2 ⭃ fc sigma 1
 
 (⇢) ∷ a → b → Sample a b
 (⇢) = (,)
@@ -24,9 +28,34 @@ samples = [
 
 main ∷ IO ()
 main = do
-	n ← test
+	n ← nnet
 	(e, n') ← runLearnT n $ trainUntil 1.0 10000 4 0.0001 squared samples
 	liftIO $ putStrLn $ "error: " ++ show e
 	liftIO $ putStrLn "result:"
 	liftIO $ forM_ samples $ \(x, _) →
 		putStrLn $ show x ++ " -> " ++ show (eval n' x)
+
+trainUntil' ∷ Rational → Int → Vector Double → Cost (Vector Double) → [Sample (Vector Double) (Vector Double)] → Net Double → IO (Net Double)
+trainUntil' λ batch eps c xs n = liftM snd $ runLearnT n $ iterateUntil (< eps) $ do
+	xs' ← shuffleList xs
+	e ← liftM avg $ replicateM 10 $ trainEpoch λ c $ makeBatches batch xs'
+	liftIO $ print e
+	return e
+
+learnUnary ∷ (Double → Double) → IO (Net Double)
+learnUnary fn = do
+	n ← net $ input 1 ⭃ fc sigma 5 ⭃ fc sigma 5 ⭃ fc sigma 5 ⭃ fc sigma 5 ⭃ fc sigma 1
+	let
+		fn' v = vector [fn (v ! 0)]
+		args = map vector $ replicateM 1 [0.0, 0.1 .. 1.0]
+		smps = args `zip` map fn' args
+	trainUntil' 1.0 10 1e-4 squared smps n
+
+learnBinary ∷ (Double → Double → Double) → IO (Net Double)
+learnBinary fn = do
+	n ← net $ input 2 ⭃ fc sigma 5 ⭃ fc sigma 5 ⭃ fc sigma 5 ⭃ fc sigma 5 ⭃ fc sigma 1
+	let
+		fn' v = vector [fn (v ! 0) (v ! 1)]
+		args = map vector $ replicateM 2 [0.0, 0.1 .. 1.0]
+		smps = args `zip` map fn' args
+	trainUntil' 1.0 10 1e-4 squared smps n
